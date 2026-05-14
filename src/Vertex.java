2d920171a9840b0803eb5d8fcc98186fcd8f8e40
Dmitriy <feljer1598@mail.ru>
2026-05-14 12:38:18 +0600

    vertex + edge

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,2 +1,12 @@
 public class Vertex {
+    private int id;
+    public Vertex(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public String toString() {
+        return "Vertex " + id;
+    }
 }
